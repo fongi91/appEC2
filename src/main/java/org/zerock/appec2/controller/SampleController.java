1a4cb9569f94ee2ff63c5654e7e98204c20d7f6a
DESKTOP-QOF5SIA\human-09 <hongwk0315@gmail.com>
2025-03-10 16:10:14 +0900

    테스트 배포2

--- src/main/java/org/zerock/appec2/controller/SampleController.java
+++ src/main/java/org/zerock/appec2/controller/SampleController.java
@@ -1,4 +1,4 @@
-package org.zerock.appec2.Controller;
+package org.zerock.appec2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
